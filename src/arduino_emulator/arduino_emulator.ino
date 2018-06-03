c9c6857acfa940e69138867ff44ede83b7aa984e
peerfunk <26229643+peerfunk@users.noreply.github.com>
2018-06-03 17:43:40 +0200

    added solo arduino emulator

---
+++ src/arduino_emulator/arduino_emulator.ino
@@ -0,0 +1,20 @@
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  
+}
+
+
+void loop() {
+ Serial.println("Name:  40BANDEL");
+ Serial.println("ID:    40");
+ Serial.println("Holder:");
+ Serial.println("Specie:Dog");
+ Serial.println("cCRP:  124.0 mg/l");
+ Serial.println("Range: 0.0 - 10.0");
+ Serial.println("Specie:Dog");
+ Serial.println("Time:  13:01");
+ Serial.println("Date:  21-01-2018");
+ delay(1000);
+}
